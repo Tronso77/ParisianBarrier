62c7ad52333279e401d1d01f7fc9b42a93b0169f
Fab <88029254+Tronso77@users.noreply.github.com>
2025-05-07 23:13:11 +0100

    #

--- src/models/simulator.py
+++ src/models/simulator.py
@@ -155,6 +155,7 @@ def _simulate_cgmy(params, nsteps, nsim, dt, trunc_eps=1e-4):
     # Approximate Lévy measure truncation
     # Simulate compound Poisson process for large jumps
     jump_rate = C * gamma(1 - Y) * ((G ** (Y - 1)) + (M ** (Y - 1)))
+    jump_rate = abs(jump_rate)
     num_jumps = np.random.poisson(jump_rate * T, size=nsim)
 
     paths = np.zeros((nsteps + 1, nsim))
